fef8a097fa6e684b0d35c4d5f0d999e1c2a0486f
Joffily Ferreira <joffily@outlook.com>
2017-10-31 20:05:54 -0300

    Adiciona exemplo de testes para view, template e models :fire:

--- rh/tests.py
+++ rh/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from model_mommy import mommy
+from rh.models import Perfil
 
-# Create your tests here.
+
+class RHTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.form_data = {
+            "user-username": "tester",
+            "user-password1": "testerrandompass987",
+            "user-password2": "testerrandompass987",
+            "perfil-cidade": "some city"
+        }
+
+    def test_perfil_model(self):
+        perfil = mommy.make(Perfil)
+        self.assertEqual("Perfil do usuário: {}".format(perfil.user.pk), str(perfil))
+
+    def test_access_view(self):
+        response = self.client.get(reverse("rh:user_perfil_create"))
+        self.assertEqual(200, response.status_code)
+
+    def test_create_user_with_profile(self):
+        response = self.client.post(reverse("rh:user_perfil_create"),
+                                    self.form_data, follow=True)
+        self.assertIn("User created successfully", response.content.decode())
+        self.assertRedirects(response, reverse("rh:user_perfil_create"))
+
+
--- rh/views.py
+++ rh/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.urls import reverse
 from rh.forms import UserPerfilForm
-
 
 
 def user_perfil_create(request):
@@ -20,7 +21,7 @@ def user_perfil_create(request):
             perfil = perfil_form.save(commit=False)
             perfil.user = user
             perfil.save()
-
-
+            messages.success(request, "User created successfully")
+            return redirect(reverse("rh:user_perfil_create"))
 
     return render(request, 'user_perfil_create.html', locals())
